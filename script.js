c54c8e5f156fe662513c08ae26c3ce7f63919715
grazeex <grazielelanna@gmail.com>
2025-02-14 11:49:07 -0300

    Alterações do código referente a atividade

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    let palavra = str.toLowerCase().replace(/\s/g, '')
+    let invertida = ""
+   
+    for(let i = palavra.length - 1; i >= 0; i--){
+        invertida += palavra[i]     
+    }
+    return palavra === invertida
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+console.log(isPalindrome("Radar"))
+
+
+
+
+function arrayMaxMin(array){
+    let minima = array[0];
+    let maxima = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minima) {
+            minima = array[i];
+        }
+        if (array[i] > maxima) {
+            maxima = array[i];
+        }
+    }
+
+    return [minima, maxima];
 }
+   console.log(arrayMaxMin([3, 1, 7, 9, 2, 10]))
+    
+
